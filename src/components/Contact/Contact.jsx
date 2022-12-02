dc29689fc7c7da2b2d70920e6644a768793a5898
balaji <kbalaji15j@gmail.com>
2022-12-02 11:25:41 +0530

    contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -11,21 +11,20 @@ const Contact = () => {
 
       <div className='cointainer contact__container'>
         <div className='contact__options'>
-        </div>
-        <article className='contact__option'>
-          <MdOutlineEmail className='contact__option-icon'/>
-          <h4>Email</h4>
-          <h5>kbalji15j@gmail.com</h5>
-          <a href ="mailto:kbalaji15j@gmail.com" target="_blank">Send a Message</a>
+             <article className='contact__option'>
+              <MdOutlineEmail className='contact__option-icon'/>
+               <h4>Email</h4>
+                <h5>kbalji15j@gmail.com</h5>
+              <a href ="mailto:kbalaji15j@gmail.com" target="_blank">Send a Message</a>
 
-        </article>
-        <article className='contact__option'>
+                </article>
+         <article className='contact__option'>
           <BsInstagram className='contact__option-icon'/>
           <h4>Instagram</h4>
           <h5>sleepy fellow_91221</h5>
           <a href ="msgto:https://www.instagram.com/sleepy_fellow_91221/" target="_blank">Send a Message</a>
 
-        </article>
+        </article> 
         <article className='contact__option'>
           <BsWhatsapp className='contact__option-icon'/>
           <h4>Whatsapp</h4>
@@ -33,6 +32,7 @@ const Contact = () => {
           <a href ="https://api.whatsapp.com/send?phone=+919398806613" target="_blank">Send a Message</a>
 
         </article>
+        </div>
           <form action="">
             <input type="text" name="name" placeholder='your full name' required/>
             <input type="email" name="email" placeholder='Email' required/>
